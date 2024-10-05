2765d8e860b68d69110d1e0c202311de5b45b5a4
Alexandr Gyach <as.gyach@gmail.com>
2024-10-05 11:19:43 +0300

    feat: added service locator

---
+++ Assets/_Source/ServiceLoacatorSystem/ServiceLocator.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+
+namespace ServiceLocatorSystem
+{
+    public class ServiceLocator
+    {
+        private readonly Dictionary<Type, IService> _services;
+
+        public ServiceLocator()
+        {
+            _services = new Dictionary<Type, IService>();
+        }
+
+        public void Register<T>(T service)
+            where T : IService
+        {
+            var key = typeof(T);
+            if (_services.ContainsKey(key))
+            {
+                UnityEngine.Debug.LogError($"Service {nameof(key)} already registered");
+            }
+
+            _services.Add(key, service);
+        }
+
+        public void Unregister<T>()
+            where T : IService
+        {
+            var key = typeof(T);
+            if (!_services.ContainsKey(key))
+            {
+                UnityEngine.Debug.LogError($"Service {key} not registered");
+            }
+
+            _services.Remove(key);
+        }
+
+        public T GetService<T>()
+            where T : IService
+        {
+            var key = typeof(T);
+            if (!_services.ContainsKey(key))
+            {
+                UnityEngine.Debug.LogError($"Service {key} not registered");
+            }
+
+            return (T)_services[key];
+        }
+    }
+}
