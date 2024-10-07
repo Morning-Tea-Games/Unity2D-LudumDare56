02d2f4478215d6363ce2db8a54402f535cbb11ff
Alexandr Gyach <as.gyach@gmail.com>
2024-10-07 17:10:06 +0300

    feat: added background parallax effect

---
+++ Assets/_Source/Entities/BackgroundParallax.cs
@@ -0,0 +1,31 @@
+using GameData;
+using Services;
+using UnityEngine;
+
+public class BackgroundParallax : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _fore;
+
+    [SerializeField]
+    private Transform _middle;
+
+    [SerializeField]
+    private Transform _back;
+
+    private Transform _playerTransform;
+
+    private void Start()
+    {
+        _playerTransform = ServiceController_Game
+            .ServiceLocator.GetService<PlayerTransformService>()
+            .GetPlayerTransform();
+    }
+
+    private void Update()
+    {
+        _fore.position = _playerTransform.position / 5f;
+        _middle.position = _playerTransform.position / 15f;
+        _back.position = _playerTransform.position / 25f;
+    }
+}
